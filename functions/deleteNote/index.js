640c2b3a81944c83893fddfea7b074cc5704bbc9
Antonio Saracino <antonio.saracino83@gmail.com>
2023-11-24 18:53:07 +0100

    Delete note error handling and security checks

--- functions/deleteNote/index.js
+++ functions/deleteNote/index.js
@@ -9,15 +9,43 @@ const deleteNote = async (event, context) => {
     return sendResponse(401, { success: false, message: "Invalid token" });
   }
 
-  const { noteId } = event.pathParameters;
+  const body = JSON.parse(event.body);
+  const noteId = body.id;
+
+  console.log("Note ID: ", noteId);
+  console.log("Event: ", event);
+
+  if (!noteId) {
+    return sendResponse(400, {
+      success: false,
+      message: "ID not available, can't delete note",
+    });
+  }
+
+  const params = {
+    TableName: "note-db", 
+    Key: {
+      id: noteId,
+    },
+  };
 
   try {
-    await db
-      .delete({
-        TableName: "note-db",
-        Key: { id: noteId },
-      })
-      .promise();
+
+
+    const note = await db.get(params).promise();
+
+    if (!note.Item) {
+      return sendResponse(404, { success: false, message: "Note not found" });
+    }
+
+    if (note.Item.userId != event.id) {
+      return sendResponse(404, {
+        success: false,
+        message: "Note does not belong to current user",
+      });
+    }
+
+    await db.delete({ TableName: "note-db", Key: { id: noteId } }).promise();
 
     return sendResponse(200, { success: true, message: "Note deleted" });
   } catch (error) {
